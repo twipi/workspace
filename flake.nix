3cbb278e717ff9564846497889f743b7afbe9e78
diamondburned <diamond@libdb.so>
2024-05-07 03:50:04 -0700

    Separate dev and prod environments

--- flake.nix
+++ flake.nix
@@ -35,7 +35,11 @@
         mkJSONConfig = name: config: pkgs.writeText name (builtins.toJSON config);
 
         mkProcessesConfig = (
-          { basePort, stateDirectory }:
+          {
+            twipiURL ? null,
+            basePort,
+            stateDirectory,
+          }:
           with lib;
           with builtins;
           rec {
@@ -44,7 +48,8 @@
               command = "task dev";
               healthPath = "/";
               environment = {
-                TWIPI_URL = "http://localhost:${toString twipi.port}";
+                TWIPI_URL =
+                  if twipiURL != null then "https://${twipiURL}" else "http://localhost:${toString twipi.port}";
               };
               dependsOn = [ "twipi" ];
             };
@@ -53,7 +58,11 @@
               command = "task dev";
               healthPath = "/";
               environment = {
-                WSBRIDGE_URL = "ws://localhost:${toString twipi.port}/sms/ws";
+                WSBRIDGE_URL =
+                  if twipiURL != null then
+                    "wss://${twipiURL}/sms/ws"
+                  else
+                    "ws://localhost:${toString twipi.port}/sms/ws";
                 WSBRIDGE_NUMBER_SELF = head clientPhoneNumbers;
                 WSBRIDGE_NUMBER_SERVER = head serverPhoneNumbers;
               };
@@ -121,87 +130,99 @@
           }
         );
 
-        processComposeFile = mkJSONConfig "process-compose.json" (
-          let
-            stateDirectory = "/tmp/twipi";
-            processesConfig = mkProcessesConfig {
-              basePort = 5000;
-              inherit stateDirectory;
-            };
-            mapDependsOn =
-              processes: condition:
-              builtins.listToAttrs (map (process: lib.nameValuePair process { inherit condition; }) processes);
-          in
-          {
-            version = "0.5";
-            processes = lib.mapAttrs' (name: process: {
-              inherit name;
-              value = (
+        processComposeFile =
+          args:
+          mkJSONConfig "process-compose.json" (
+            let
+              stateDirectory = "/tmp/twipi";
+              processesConfig = mkProcessesConfig (
                 {
-                  working_dir = "./" + (process.workingDirectory or name);
-                  command =
-                    let
-                      script = pkgs.writeShellScript "${name}.sh" ''
-                        set -ex
-                        mkdir -p "$STATE_DIRECTORY"
-                        ${process.command}
-                      '';
-                    in
-                    "nix develop -c ${script}";
-                  environment = lib.mapAttrsToList (k: v: "${k}=${v}") (
-                    (process.environment or { })
-                    // (lib.optionalAttrs (process ? "port") { PORT = toString process.port; })
-                    // ({ STATE_DIRECTORY = stateDirectory + "/" + name; })
-                  );
-                  shutdown = {
-                    signal = 2; # SIGINT
-                    timeout_seconds = 5;
-                  };
-                  depends_on =
-                    { }
-                    // (mapDependsOn (process.after or [ ]) "process_completed_successfully")
-                    // (mapDependsOn (process.dependsOn or [ ]) "process_healthy");
+                  basePort = 5000;
+                  inherit stateDirectory;
                 }
-                // (lib.optionalAttrs (process ? "healthPath") {
-                  readiness_probe = {
-                    http_get = {
-                      host = "localhost";
-                      port = process.port;
-                      path = process.healthPath;
-                    };
-                    period_seconds = 1;
-                    failure_threshold = 300;
-                  };
-                  availability = {
-                    restart = "exit_on_failure";
-                    backoff_seconds = 2;
-                  };
-                })
+                // args
               );
-            }) processesConfig;
-          }
-        );
+              mapDependsOn =
+                processes: condition:
+                builtins.listToAttrs (map (process: lib.nameValuePair process { inherit condition; }) processes);
+            in
+            {
+              version = "0.5";
+              processes = lib.mapAttrs' (name: process: {
+                inherit name;
+                value = (
+                  {
+                    working_dir = "./" + (process.workingDirectory or name);
+                    command =
+                      let
+                        script = pkgs.writeShellScript "${name}.sh" ''
+                          set -ex
+                          mkdir -p "$STATE_DIRECTORY"
+                          ${process.command}
+                        '';
+                      in
+                      "nix develop -c ${script}";
+                    environment = lib.mapAttrsToList (k: v: "${k}=${v}") (
+                      (process.environment or { })
+                      // (lib.optionalAttrs (process ? "port") { PORT = toString process.port; })
+                      // ({ STATE_DIRECTORY = stateDirectory + "/" + name; })
+                    );
+                    shutdown = {
+                      signal = 2; # SIGINT
+                      timeout_seconds = 5;
+                    };
+                    depends_on =
+                      { }
+                      // (mapDependsOn (process.after or [ ]) "process_completed_successfully")
+                      // (mapDependsOn (process.dependsOn or [ ]) "process_healthy");
+                  }
+                  // (lib.optionalAttrs (process ? "healthPath") {
+                    readiness_probe = {
+                      http_get = {
+                        host = "localhost";
+                        port = process.port;
+                        path = process.healthPath;
+                      };
+                      period_seconds = 1;
+                      failure_threshold = 300;
+                    };
+                    availability = {
+                      restart = "exit_on_failure";
+                      backoff_seconds = 2;
+                    };
+                  })
+                );
+              }) processesConfig;
+            }
+          );
+
+        processCompose =
+          args:
+          pkgs.writeShellApplication rec {
+            name = "twipi-dev";
+            meta.mainProgram = "twipi-dev";
+            runtimeInputs = [ pkgs.process-compose ];
+            text = ''
+              exec process-compose up \
+                --config ${processComposeFile args} \
+                --ref-rate 100ms \
+                --no-server \
+                --keep-tui \
+                --ordered-shutdown
+            '';
+          };
       in
       {
-        packages.default = pkgs.writeShellApplication rec {
-          name = "twipi-dev";
-          meta.mainProgram = "twipi-dev";
-          runtimeInputs = [ pkgs.process-compose ];
-          text = ''
-            exec process-compose up \
-              --config ${processComposeFile} \
-              --ref-rate 100ms \
-              --no-server \
-              --keep-tui \
-              --ordered-shutdown
-          '';
+        packages = rec {
+          dev = processCompose { };
+          prod = processCompose { twipiURL = "twipi.libdb.so"; };
         };
 
         devShells.default = pkgs.mkShell {
           packages = [
             self.formatter.${system}
             pkgs.go_1_22
-            (pkgs.writeShellScriptBin "run" "nix run .?submodules=1#")
+            (pkgs.writeShellScriptBin "run" "nix run .?submodules=1#dev")
           ];
         };
 
